21ff4b71d3c618f91f98a64014c713c72c26cfaf
Anil Tanukula <aniltanukula3010@gmail.com>
2025-11-01 10:51:17 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH BIKE as (
+
+
+     select distinct
+     START_STATIO_ID as station_id,
+    start_station_name as station_name,
+    start_lat as station_lat,
+    start_lng as start_station_lng
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID <>'ride_id'
+)
+select * from BIKE
